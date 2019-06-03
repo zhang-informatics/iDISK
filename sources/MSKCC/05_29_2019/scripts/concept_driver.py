efce8e8aec81a891d0a01f1084f1a15e52214722
changye_umn <lixx3013@umn.edu>
2019-06-03 16:37:21 -0500

    fix multiple values for argument rel_name TypeError

--- sources/MSKCC/05_29_2019/scripts/concept_driver.py
+++ sources/MSKCC/05_29_2019/scripts/concept_driver.py
@@ -16,52 +16,51 @@ class concept_driver(object):
         self.path = os.path.join(self.path, "download")
     # read cancer_ann_data.jsonl file
     def readFile(self):
-        # list to store multiple concepts
         # TODO: discuss with Jake about storing options
-        concepts = []
-        count = 0
         with open(os.path.join(self.path, "cancer_ann_data.jsonl"), "r") as readFile:
             for line_number, line in enumerate(readFile):
                 herb = json.loads(line)
                 herb_id = line_number
                 herb_name = herb["name"]
-                # for each ingredient
+                # for each annotated HDI
                 herb_atom = Atom(herb_name, src = "MSKCC", src_id = str(herb_id), 
                             term_type = "SY", is_preferred = True)
                 # TODO: no definition for concept_type
                 herb_concept = Concept.from_atoms([herb_atom], concept_type = "SDSI")
                 annHDI = herb["annotated_HDI"]
                 # HDI: interacts_with
-                self.generateRelationship(herb_concept, annHDI, count, "interacts_with")
+                self.generateRelationship(herb_concept, annHDI, 
+                                            "interacts_with", "UMLS")
                 # ADR: has_adverse_effect_on
                 annADR = herb["annotated_ADR"]
-                self.generateRelationship(herb_concept, annADR, count, "has_adverse_effect_on")
+                self.generateRelationship(herb_concept, annADR, 
+                                            "has_adverse_effect_on", "MEDDRA")
 
     # generate annotated HDI relationships for a single herb
     # @herb_concept: herb concept class from readFile function
     # @ann: list of annotated json object from cancer_ann_data.jsonl
-    # @count: counter as src_id
-    # @type: relationship type
-    def generateRelationship(self, herb_concept, ann, count, type):
+    # @rel_type: relationship type
+    # @src_type: srouce type, i.e. "UMLS" or "MEDDRA"
+    def generateRelationship(self, herb_concept, ann, rel_type, src_type):
         if isinstance(ann, list):
             # remove empty entry
             ann = [x for x in ann if x != " "]
-            herb_concept.attributes = []
+            # list to store Attribute Atoms
+            attr_atoms = []
             for each in ann:
-                atom = Atom(each["term"], src = "MSKCC", 
-                            src_id = str(count), term_type = "SY", is_preferred = True)
-                count += 1
-                hdi_concept = Concept.from_atoms([atom], concept_type = "SDSI")
-                hdi_attr = Attribute(hdi_concept, atr_name = "annotated_HDI",
-                                    atr_value = json.dumps(each), src = "UMLS")
-                herb_concept.attributes.append(hdi_attr)
-                hdi_rel = Relationship(herb_concept, hdi_concept, rel_name = type, src = "MSKCC")
-                herb_concept.relationships.append(hdi_rel)
+                atom = Atom(each["term"], src = "MSKCC", src_id = str(each["id"]), 
+                            term_type = "SY", is_preferred = True)
+                attr_atoms.append(atom)
+            # generate Concept for the Atom(s)
+            attr_concept = Concept.from_atoms(attr_atoms, concept_type = "SDSI")
+            # generate Relationship for attr_concept Concept
+            attr_rel = Relationship(subject = herb_concept, obj = attr_concept, 
+                                    rel_name = rel_type, src = src_type)
+            # add attr_rel to the original concept
+            herb_concept.relationships = [attr_rel]
         else:
             pass
 
-
-        
-
-x = concept_driver()
-x.readFile()
+if __name__ == "__main__":
+    x = concept_driver()
+    x.readFile()
