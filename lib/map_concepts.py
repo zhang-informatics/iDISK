7ada7f0bc57f8e20bfa719c3080c7c5f39d9c4f8
Jake Vasilakes <vasil024@umn.edu>
2019-06-20 17:02:56 -0500

    Updated metamap driver to handle full text as well as terms

--- lib/map_concepts.py
+++ lib/map_concepts.py
@@ -7,11 +7,12 @@ from collections import defaultdict
 from py2neo import Graph
 
 from mappers import MetaMapDriver
+from idlib import Concept
 
 
 """
 This script performs all required mapping to existing terminologies
-as specified in the iDISK schema. 
+as specified in the iDISK schema.
 """
 
 
@@ -50,7 +51,7 @@ class Schema(object):
 
     def __init__(self, uri, user, pswd, cypher_file=None):
         self.graph = self._get_graph(uri, user, pswd)
-        # Create the graph is a schema was specified.
+        # Create the graph if a schema was specified.
         # Otherwise, use the existing graph at the uri.
         if cypher_file is not None:
             self._create_schema(cypher_file)
@@ -81,7 +82,8 @@ class Schema(object):
         :param str cypher_file: Path to a Cypher file that defines the schema.
         """
         if len(self.graph.nodes) > 0:
-            raise ValueError(f"Cypher file specified but the graph is not empty. Aborting.")  # noqa
+            msg = "Cypher file specified but the graph is not empty. Aborting."
+            raise ValueError(msg)
         cyp = open(cypher_file, 'r').read()
         self.graph.run(cyp)
 
@@ -100,10 +102,16 @@ class Schema(object):
                 terms.add(node["maps_to"])
         return terms
 
-    def node_from_label(self, label):
+    def get_node_from_label(self, label):
+        """
+        :param str label: The label to use as a lookup.
+        """
         return self.graph.nodes.match(label).first()
 
-    def rel_from_name(self, rel_name):
+    def get_relationship_from_name(self, rel_name):
+        """
+        :param str rel_name: The relationship name.
+        """
         return self.graph.relationships.match(r_type=rel_name).first()
 
 
@@ -135,6 +143,52 @@ def get_annotators(annotator_ini, schema):
     return annotators
 
 
+def build_mapping_queries(annotators, concepts, schema):
+    strings_to_map = defaultdict(list)
+
+    new_concept_counter = 0
+    counter_template = "UNK{0.07}"
+    for concept in concepts:
+        data_element = schema.get_node_from_label(concept.concept_type)
+        concept_terminology = data_element["maps_to"]
+        strings_to_map[concept_terminology].append((concept.ui,
+                                                    concept.preferred_term))
+
+        for rel in concept.get_relationships():
+            rel_graph = schema.get_relationship_from_name(rel.rel_name)
+            if rel_graph.end_node() is None:
+                warnings.warn(f"Relationship '{rel.rel_name}' not in schema.")
+                continue
+            rel_terminology = rel_graph.end_node()["maps_to"]
+            string_id = counter_template.format(new_concept_counter)
+            new_concept_counter += 1
+            strings_to_map[terminology].append((string_id, rel.rel_value))
+
+    return strings_to_map
+
+
+def map_strings_to_terminologies(strings_to_map, annotators):
+    mappings = {}
+    for terminology in strings_to_map.keys():
+        ann = annotators[terminology]
+        query = '\n'.join([f"{sid}|{string}" for (sid, string)
+                           in strings_to_map[terminology].items()])
+        candidates = ann.map(query)
+        mappings[terminology] = ann.get_best_mappings(candidates)
+    return mappings
+
+
+def create_concepts_from_mappings(mappings, existing_concepts):
+    num_atoms = sum([1 for atom in concept.get_atoms()
+                     for concept in existing_concepts])
+    Atom.init_counter(num_atoms)
+    Concept.init_counter(len(existing_concepts))
+    for terminology in mappings.keys():
+        for (sid, mappings) in mappings[terminology].items():
+            orig_str_atom = Atom(
+
+
+        
 def map_concepts(annotators, concepts_file, outfile, schema):
     with open(concepts_file, 'r') as inF:
         inlines = [json.loads(line) for line in inF]
