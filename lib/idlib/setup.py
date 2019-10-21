4d9f11f2e7e38ea2a968a2937f37eab1338e740a
Jake Vasilakes <vasil024@umn.edu>
2019-10-21 17:10:43 -0500

    Add idlib to setup.py

--- lib/idlib/setup.py
+++ lib/idlib/setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="",
-    packages=setuptools.find_packages(),
+    packages=["idlib"] + setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License"
