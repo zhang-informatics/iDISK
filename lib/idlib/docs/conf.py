9d839b71eab1274f6b470ccb4e5abed9dc7c4492
Jake Vasilakes <vasil024@umn.edu>
2019-10-21 17:07:07 -0500

    Remove sphinx_markdown_builder requirement.

--- lib/idlib/docs/conf.py
+++ lib/idlib/docs/conf.py
@@ -42,7 +42,6 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
-    'sphinx_markdown_builder'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
