ca143dc129606c3deb1bda16bf4f7ae740598359
Jake Vasilakes <vasil024@umn.edu>
2019-10-22 12:10:29 -0500

    Version update

--- lib/idlib/docs/conf.py
+++ lib/idlib/docs/conf.py
@@ -26,7 +26,7 @@ author = 'Jake Vasilakes'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '1.0.1'
 
 
 # -- General configuration ---------------------------------------------------
