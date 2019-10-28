df24583f031caa0d17e2afd3c5bcff9506e4f2c7
Jake Vasilakes <vasil024@umn.edu>
2019-10-28 13:06:57 -0500

    Add function to load a specific KB version.

--- lib/idlib/idlib/__init__.py
+++ lib/idlib/idlib/__init__.py
@@ -4,12 +4,14 @@ import os  # noqa
 from .schema import Schema  # noqa
 
 
-def load_kb(version_dir):
+def load_environment(version_dir):
     config_file = os.path.join(version_dir, ".config")
     for line in open(config_file, 'r'):
         var, value = line.split('=')
         os.environ[var] = value.strip()
 
+def load_kb(version_dir):
+    load_environment(version_dir)
     from idlib.data_elements import Concept
     concepts_file = os.path.join(version_dir, "concepts/concepts_merged.jsonl")
     kb = Concept.read_jsonl_file(concepts_file)
